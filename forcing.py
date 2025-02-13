02da44af5768c5319864debbbceb567cab4b6253
Richard Buscaglia <rb821@ic.ac.uk>
2025-02-13 09:36:30 +0000

    before neumann

---
+++ forcing.py
@@ -0,0 +1,17 @@
+class forcing:
+    '''
+    This class is used to hold the forcing function data
+    
+    Forcing Functions available:
+    1. Step Function
+    2. Sine Function
+    3. Impulse Function
+    
+    '''
+  
+    def __init__(self):
+        pass
+    
+    
+    def sine(self):
+        
